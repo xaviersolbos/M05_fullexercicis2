55f0b524cf1a05d58a9c7e55f17c90fff861d7e2
1r_DAM <1r_DAM@DESKTOP-SG5OKMH>
2022-11-03 15:41:29 +0100

    Valor de la moneda canviat

---
+++ src/Conversio1.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+public class Conversio1 {
+      public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introdueix en €");
+        int valor_euros = sc.nextInt();
+                
+        double tipus_de_canvi = 0.87d;
+        System.out.println(valor_euros + " eur0s (€) acatualmentt ewuivalen a" + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+    }
+}
